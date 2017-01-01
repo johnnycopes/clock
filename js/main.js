3a3c143f48b7cea50d2c568a80c445249c5fd47d
John Coppola <jdcoppola@gmail.com>
2016-12-31 21:22:54 -0500

    finishes basic clock

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
